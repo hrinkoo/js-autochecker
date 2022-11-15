5fd5f549527efe8a84c1397d128f58706e500e4c
Bohdan Hrinko <bohdan.hrinko@gmail.com>
2022-11-15 21:11:05 +0200

    Додає налаштування до js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// console.log(!true); // false
+// console.log(!false); // true
+// console.log(!3); // !3 -> !true -> false
+// console.log(!"Mango"); // !"Mango" -> !true -> false
+// console.log(!0); // !0 -> !false -> true
+// console.log(!""); // !"" -> !false -> true
+
+// const isOnline = true;
+// const isNotOnline = !isOnline; // !isOnline -> !true -> false
